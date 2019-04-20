c8a76f93dd5b1bc98b5a4b5261330224a9adf5c8
Wesley Camargo <camargo.wes@gmail.com>
2019-04-20 12:57:24 -0300

    remocao template app insights

--- dotnetcoreApp-Template/Deploy-AzureResourceGroup.ps1
+++ dotnetcoreApp-Template/Deploy-AzureResourceGroup.ps1
@@ -6,8 +6,8 @@ Param(
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = 'WebSite.json',
-    [string] $TemplateParametersFile = 'WebSite.parameters.json',
+    [string] $TemplateFile = 'WebApp.json',
+    [string] $TemplateParametersFile = 'WebApp.parameters.json',
     [string] $ArtifactStagingDirectory = '.',
     [string] $DSCSourceFolder = 'DSC',
     [switch] $ValidateOnly
